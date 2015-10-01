6d19829f60b203b457257059dbdd9952bdeb0e15
lapico <developer@ak6.ch>
2015-10-01 17:33:30 +0200

    comment uglifier

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
   config.assets.debug = false
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
